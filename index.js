70a980d56041c0fb8b3927ff175ad90a8eb8d0e3
Edmond Meinfelder <edmond@stdarg.com>
2014-01-12 15:26:22 -0800

    First commit.

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict';
+var have = require('have');
+var npm = require('npm');
+var asyncerr = require('async-err');
+var inspect = require('util').inspect;
+var semver = require('semver');
+
+module.exports = function (name, cb) {
+    have(arguments, { name: 'string', cb: 'function' });
+
+    if (!name.length)
+        return asyncerr(new Error('Bad name parameter, empty string.', cb));
+
+    npm.load({ loglevel: 'silent' }, function (err) {
+        // A hack to shut the NPM registry the hell up.
+        if (npm && npm.registry && npm.registry.log && npm.registry.log.level)
+            npm.registry.log.level = 'silent';
+
+        if (err) return cb(err);
+
+        var silent = true;      // make npm not chatty on stdout
+        npm.commands.view([name], silent, function (err, data) {
+            if (err) return cb(err);
+            if (!data) return cb(new Error('No data received.'));
+
+            for (var p in data) {
+                if (!data.hasOwnProperty(p) ||!semver.valid(p))
+                    continue;
+                return cb(null, p, data[p]);
+            }
+            return cb(new Error('Bad data received: '+inspect(data)));
+        });
+    });
+};
---
+++ test.js
@@ -0,0 +1,48 @@
+'use strict';
+var assert = require('assert');
+var npmview = require('./index');
+var inspect = require('util').inspect;
+var semver = require('semver');
+
+describe('npm view', function() {
+    it('Should throw an exception for missing name argument', function(done) {
+        try {
+            npmview();
+        } catch(err) {
+            assert.ok(err.message === 'name argument is not string');
+            done();
+        }
+    });
+
+    it('Should throw an exception for missing callback argument', function(done) {
+        try {
+            npmview('is2');
+        } catch(err) {
+            assert.ok(err.message === 'cb argument is not function');
+            done();
+        }
+    });
+    it('Should return valid data for a module in npm', function(done) {
+        npmview('is2', function(err, version, moduleInfo) {
+            if (err) {
+                console.log(err);
+                return done(err);
+            }
+
+            if (!moduleInfo) {
+                return done(new Error('no module information received.'));
+            }
+            if (!semver.valid(version)) {
+                return done(new Error('Invalid module version: '+version));
+            }
+            var description = 'A type checking library where each exported '+
+                'function returns either true or false and does not throw. '+
+                'Also added tests.';
+            if (moduleInfo.description !== description) {
+                return done(new Error('unexpected response: '+inspect(moduleInfo)));
+            }
+
+            done();
+        });
+    });
+});
